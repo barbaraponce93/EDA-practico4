3e3bd13df595a2fd42403f685857c200820db93e
barbaraponce93 <141653104+barbaraponce93@users.noreply.github.com>
2024-05-23 10:59:43 -0300

    Add files via upload

---
+++ Practico4/ejercicioC/ejercicioC.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+/*Hacer una función que reciba un puntero y compruebe si el número es par o impar, y señalar la posición de
+memoria donde se está guardando el número*/
+
+
+
+return 0;
+}
