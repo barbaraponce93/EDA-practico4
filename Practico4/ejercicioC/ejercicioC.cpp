695975c99ab64381195c7444e0c9ee29bf036f18
Bárbara Ponce <barbaraponce93@gmail.com>
2024-06-02 13:08:23 -0300

    c

--- Practico4/ejercicioC/ejercicioC.cpp
+++ Practico4/ejercicioC/ejercicioC.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
-
+#include<locale.h>
 using namespace std;
 
+void esParOImpar(int* a){
+	if(*a%2==0){
+		cout<< "El número "<<*a<<" es par"<<endl;
+	
+	}
+	else{  
+		cout<< "El número "<<*a<<" es impar"<<endl;
+			
+	}
+	
+		cout<<"Posición: "<<a<<endl;
+	
+}
 
 int main(){
-/*Hacer una función que reciba un puntero y compruebe si el número es par o impar, y señalar la posición de
+/* C-Hacer una función que reciba un puntero y compruebe si el número es par o impar, y señalar la posición de
 memoria donde se está guardando el número*/
+	setlocale(LC_ALL,"Spanish");
+int numero;
 
-
+ 		cout<<"Ingrese un número"<<endl;
+ 		cin>>numero;
+ 		
+ 		esParOImpar(&numero);// con & obtengo la direccion de memoria de la variable
+ 		
 
 return 0;
 }
