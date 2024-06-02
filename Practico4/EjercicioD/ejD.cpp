04d268fa8c7984e51fd27e0a07efca497cca7b85
NahuelRoman1990 <nahuel.90.roman@gmail.com>
2024-06-02 19:41:32 -0300

    Ejercicio D

--- Practico4/EjercicioD/ejD.cpp
+++ Practico4/EjercicioD/ejD.cpp
@@ -1,20 +1,37 @@
+#include <stdio.h>
+#include <ctype.h>
 
-void function esParOImpar(int* a){
-	int aux=*a;
-	cout<<aux<<endl;
-	
-	
-	
-	
+void Hogwarts(char *hechizo) {
+    int vocales = 0;
+    int consonantes = 0;
+    
+ 
+    for (int i = 0; hechizo[i] != '\0'; i++) {
+        char letra = hechizo[i];
+        
+
+        if (isalpha(letra)) {
+            
+            if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' ||
+                letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U') {
+                vocales++;
+            } else {
+                consonantes++;
+            }
+        }
+    }
+    
+
+    printf("%s tiene: %d vocales, %d consonantes.\n", hechizo, vocales, consonantes);
 }
 
-int main(){
-
-
-
-
-
-
-
-return 0;
+int main() {
+    char hechizo1[] = "Expelliarmus";
+    char hechizo2[] = "Alohomora";
+    
+    Hogwarts(hechizo1); 
+    Hogwarts(hechizo2); 
+    
+    return 0;
 }
+
