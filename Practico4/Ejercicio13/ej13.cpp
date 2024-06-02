f01e8434b802ffc95558c6db49fdc0d110754e2c
Bárbara Ponce <barbaraponce93@gmail.com>
2024-06-02 13:06:51 -0300

    13 y 17

--- Practico4/Ejercicio13/ej13.cpp
+++ Practico4/Ejercicio13/ej13.cpp
@@ -1,17 +1,41 @@
+#include <iostream>
+#include<locale.h>
+#include<math.h>
+using namespace std;
 
-void function esParOImpar(int* a){
-	int aux=*a;
-	cout<<aux<<endl;
+
+
+int funpot(int potencia, int baseEntero){
+	if(potencia<0){
+		cout<<"El exponente debe ser un número entero positivo"<<endl;
+	return -1;//corto aca si el expo en negativo
+	}
+    double resultado = pow(static_cast<double>(baseEntero), potencia);//tengo que castear ya que pow acepta double
+	return resultado;
 	
-	
-	
-	
+
 }
 
 int main(){
+	setlocale(LC_ALL,"Spanish");
+	
+	
+/*Escriba una función nombrada funpot() que eleve un número entero que se le transmita a una potencia en 
+número entero positivo y despliegue el resultado. El número entero positivo deberá ser el segundo valor 
+transmitido a la función*/
 
 
+int potencia;
+int base;
+cout<<"Ingrese un número entero para la base"<<endl;
+cin>>base;
+cout<<"Ingrese un número entero positivo"<<endl;
+cin>>potencia;
 
+
+double resultado=funpot(potencia,base);
+if(resultado!=-1)
+cout<<"El resultado de "<<base<<" elevado "<<potencia<<" es:"<<resultado<<endl;
 
 
 
--- Practico4/Ejercicio17/ej17.cpp
+++ Practico4/Ejercicio17/ej17.cpp
@@ -1,15 +1,93 @@
+#include<iostream>
+#include<locale.h>
 
-void function esParOImpar(int* a){
-	int aux=*a;
-	cout<<aux<<endl;
-	
-	
-	
-	
+
+using namespace std;
+
+/*Un número es primo si solo es divisible por la unidad y por si mismo.
+ Defina una función que determina si un número es primo, debe recibir el número y retornar verdadero o falso.
+- Realice un programa que, de 2 opciones,
+ 1). Determinar si un número es primo 
+ 2). Calcular todos los números primos menores a un número ingresado por el usuario.
+ El usuario debe poder realizar estas opciones todas las veces que desee.
+*/
+
+bool esPrimo(int numeroP){
+	if(numeroP<=1){
+		return false;
+	}else if(numeroP==2){
+		return true;
+	}
+	else {
+		for(int i=2;i<numeroP;i++){
+			if(numeroP%i==0){//si es divisible por otro numero menor a el
+				return false;
+			}
+			
+		}
+			return true;
+	}
+
+}
+
+
+
+void calcularPrimosMenores(int numero) {
+    for (int i = 2; i < numero; ++i) {//recorro todos los num menores al ingresado
+        if (esPrimo(i)) {//si el num en la pos i es primo
+            cout << "Es primo " << i << " ";//lo muestro
+             cout<<endl; 
+        }
+       
+    }
+   
 }
 
 int main(){
+	
+	setlocale(LC_ALL, "Spanish");
+int numero;
+int opcion;
+bool resultado;
 
+
+do{
+cout<<"Ingrese un número entero positivo"<<endl;
+	cin>>numero;
+	
+   if (numero <= 0) {
+        cout << "Número ingresado invalido, por favor ingrese un número entero positivo" << endl;
+    
+      }else{
+	 
+			cout<<"Ingrese una opcion:"<<endl;
+cout<<"1 - Para determinar si un número es primo "<<endl;	
+cout<<"2 - Para calcular todos los números primos menores al número ingresado"<<endl;
+cout<<"3 - Para Salir"<<endl;		
+
+cin>>opcion;
+switch(opcion){
+	case 1 :    
+			
+				cout<<(esPrimo(numero)?"El número ingresado es Primo":"El número ingresado no es primo")<<endl;
+				cout<<endl;
+				break;
+	case 2 : 	
+			 	calcularPrimosMenores(numero);
+				break;
+	case 3 : 	
+				cout<<"Fin de la ejecución"<<endl;
+				numero=0;
+				break;	
+	default:
+			cout<< "La opción ingresada es incorrecta"	 <<endl;
+				break;	
+	
+
+	
+}
+}
+}while(numero>0);
 
 
 
